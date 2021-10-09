b8a502ae0670034e9f956e98a5c8e281fa8ac79d
Chung941211 <444192287@qq.com>
2021-10-09 18:17:30 +0800

    feat add typs

---
+++ src/packages/button/typs.ts
@@ -0,0 +1,48 @@
+
+import type { ExtractPropTypes, PropType } from 'vue';
+
+
+const ButtonTypes = tuple('default', 'primary', 'ghost', 'dashed', 'link', 'text');
+export type ButtonType = typeof ButtonTypes[number];
+const ButtonShapes = tuple('circle', 'round');
+export type ButtonShape = typeof ButtonShapes[number];
+
+const ButtonHTMLTypes = tuple('submit', 'button', 'reset');
+export type ButtonHTMLType = typeof ButtonHTMLTypes[number];
+
+export type LegacyButtonType = ButtonType | 'danger';
+export function convertLegacyProps(type?: LegacyButtonType): ButtonProps {
+  if (type === 'danger') {
+    return { danger: true };
+  }
+  return { type };
+}
+
+const buttonProps = () => ({
+  prefixCls: PropTypes.string,
+  type: PropTypes.oneOf(ButtonTypes),
+  htmlType: PropTypes.oneOf(ButtonHTMLTypes).def('button'),
+  shape: PropTypes.oneOf(ButtonShapes),
+  size: {
+    type: String as PropType<SizeType>,
+  },
+  loading: {
+    type: [Boolean, Object] as PropType<boolean | { delay?: number }>,
+    default: (): boolean | { delay?: number } => false,
+  },
+  disabled: PropTypes.looseBool,
+  ghost: PropTypes.looseBool,
+  block: PropTypes.looseBool,
+  danger: PropTypes.looseBool,
+  icon: PropTypes.VNodeChild,
+  href: PropTypes.string,
+  target: PropTypes.string,
+  title: PropTypes.string,
+  onClick: {
+    type: Function as PropType<(event: MouseEvent) => void>,
+  },
+});
+
+export type ButtonProps = Partial<ExtractPropTypes<ReturnType<typeof buttonProps>>>;
+
+export default buttonProps;
